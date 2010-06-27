6f6842a2a58a35c021ae4dfd21ce385d265f1c07
Tero Tilus <tero@tilus.net>
2010-06-27 18:33:01 +0300

    $ rails new kerppi -J -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '71d554577f0af949c4d1f6ecc7640d901b10d20d949ae62cbe93237bd60d560ec40400452cd6b914039f88dde11e5760ac43e8bcf1799453930d912ce868a75f'
